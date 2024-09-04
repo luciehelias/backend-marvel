1f9786eeddd72d616187d4490d808a3c55a6e1da
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-04 14:03:20 +0200

    create model comic

--- Models/Comic.js
+++ Models/Comic.js
@@ -1,2 +1,11 @@
 const mongoose = require("mongoose");
+
+const Comic = mongoose.model("Comic", {
+  title: "string",
+  description: "string",
+  thumbnail: {
+    path: "string",
+    extension: "string",
+  },
+});
 module.exports = Comic;
