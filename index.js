884ed502d0743d49a25be1123392248e85ef634b
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-04 12:41:55 +0200

    add routes and models

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 mongoose.connect("mongodb://localhost:27017/marvel");
+
+const charactersRouter = require("./Routes/characters");
+const comicsRouter = require("./Routes/comics");
+
+app.use(charactersRouter);
+app.use(comicsRouter);
 
 app.get("*", (req, res) => {
   return res.status(404).json("Not found");
