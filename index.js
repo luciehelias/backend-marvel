8f5f73c8deb2c6f99a6e8b88b86aaaf4d7755f6a
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-04 14:17:00 +0200

    add dotenv package

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
 mongoose.connect("mongodb://localhost:27017/marvel");
 
 const charactersRouter = require("./Routes/characters");
 const comicsRouter = require("./Routes/comics");
+const usersRouter = require("./Routes/users");
 
+app.use(usersRouter);
 app.use(charactersRouter);
 app.use(comicsRouter);
 
-app.get("*", (req, res) => {
-  return res.status(404).json("Not found");
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome, this is my marvel website !" });
 });
 
-app.listen(3000, () => {
+app.get("*", (req, res) => {
+  return res.status(404).json("This page doesn't exist");
+});
+
+app.listen(process.env.PORT, () => {
   console.log("Server has started");
 });
