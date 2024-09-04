f4fda033e3ffe7fe177b954f172fa944c7abdd45
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-04 12:18:20 +0200

    add mongoose

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/marvel");
 
 app.get("*", (req, res) => {
   return res.status(404).json("Not found");
