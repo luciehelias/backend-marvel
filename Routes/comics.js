4d7463651de8eb3f74641a6fb03bd079c061c8fe
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-04 15:43:11 +0200

    refactor comics

--- Routes/comics.js
+++ Routes/comics.js
@@ -7,44 +7,44 @@ const Character = require("../Models/Character");
 
 router.get("/comics", async (req, res) => {
   try {
-    if (req.query.apiKey) {
-      const user = await User.findOne({ apiKey: req.query.apiKey });
-      if (user) {
-        let filters = {};
+    if (!req.query.apiKey) {
+      return res
+        .status(400)
+        .json({ message: "Vous devez entrer votre API key" });
+    }
 
-        if (req.query.title) {
-          let title = new RegExp(req.query.title, "i");
-          filters.title = title;
-        }
+    const user = await User.findOne({ apiKey: req.query.apiKey });
+    if (!user) {
+      return res.status(400).json({ message: "Votre apiKey n'est pas valide" });
+    }
 
-        const skip = Number(req.query.skip);
-        let limit;
-        if (!req.query.limit) {
-          limit = 100;
-        } else {
-          limit = Number(req.query.limit);
-        }
-        if (limit <= 100 && limit >= 1) {
-          const results = await Comic.find(search)
-            .skip(skip)
-            .limit(limit)
-            .sort({ title: 1 });
-          const count = await Comic.countDocuments(search);
-          res.status(200).json({
-            count: count,
-            limit: limit,
-            results: results,
-          });
-        } else {
-          res
-            .status(400)
-            .json({ message: "La limite doit être comprise entre 1 et 100" });
-        }
-      } else {
-        res.status(400).json({ message: "Votre apiKey n'est pas valide" });
-      }
+    let filters = {};
+    if (req.query.title) {
+      filters.title = new RegExp(req.query.title, "i");
+    }
+
+    const skip = Number(req.query.skip);
+    let limit;
+    if (!req.query.limit) {
+      limit = 100;
     } else {
-      res.status(400).json({ message: "Vous devez entrer votre API key" });
+      limit = Number(req.query.limit);
+    }
+
+    if (limit <= 100 && limit >= 1) {
+      const results = await Comic.find(filters)
+        .skip(skip)
+        .limit(limit)
+        .sort({ title: 1 });
+
+      res.status(200).json({
+        limit,
+        results,
+      });
+    } else {
+      res
+        .status(400)
+        .json({ message: "La limite doit être comprise entre 1 et 100" });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -53,23 +53,23 @@ router.get("/comics", async (req, res) => {
 
 router.get("/comics/:characterId", async (req, res) => {
   try {
-    if (req.query.apiKey) {
-      const user = await User.findOne({ apiKey: req.query.apiKey });
-      if (user) {
-        const character = await Character.findByID(
-          req.params.characterId
-        ).populate("Comic");
-        res.json(
-          character
-            ? character
-            : { message: "Cet id ne correspond à aucun personnage Marvel" }
-        );
-      } else {
-        res.status(400).json({ message: "Votre apiKey n'est pas valide" });
-      }
-    } else {
-      res.status(400).json({ message: "Vous devez entrer votre API key" });
+    if (!req.query.apiKey) {
+      return res
+        .status(400)
+        .json({ message: "Vous devez entrer votre API key" });
     }
+
+    const user = await User.findOne({ apiKey: req.query.apiKey });
+    if (!user) {
+      return res.status(400).json({ message: "Votre apiKey n'est pas valide" });
+    }
+
+    const character = await Character.findByID(req.params.characterId).populate(
+      "Comic"
+    );
+    res.json(
+      character || { message: "Cet id ne correspond à aucun personnage Marvel" }
+    );
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -77,21 +77,19 @@ router.get("/comics/:characterId", async (req, res) => {
 
 router.get("/comics/:comicId", async (req, res) => {
   try {
-    if (req.query.apiKey) {
-      const user = await User.findOne({ apiKey: req.query.apiKey });
-      if (user) {
-        const comic = await Comic.findById(req.params.comicId);
-        res.json(
-          comic
-            ? comic
-            : { message: "Cet id ne correspond à aucun comic Marvel" }
-        );
-      } else {
-        res.status(400).json({ message: "Votre apiKey n'est pas valide" });
-      }
-    } else {
-      res.status(400).json({ message: "Vous devez entrer votre API key" });
+    if (!req.query.apiKey) {
+      return res
+        .status(400)
+        .json({ message: "Vous devez entrer votre API key" });
     }
+
+    const user = await User.findOne({ apiKey: req.query.apiKey });
+    if (!user) {
+      return res.status(400).json({ message: "Votre apiKey n'est pas valide" });
+    }
+
+    const comic = await Comic.findById(req.params.comicId);
+    res.json(comic || { message: "Cet id ne correspond à aucun comic Marvel" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
