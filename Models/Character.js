372df43f8b572af94f466d9452a70169b3c5dd91
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-04 14:05:07 +0200

    create character model

--- Models/Character.js
+++ Models/Character.js
@@ -1,2 +1,17 @@
 const mongoose = require("mongoose");
+const Character = mongoose.model("Character", {
+  name: "string",
+  description: "string",
+  thumbnail: {
+    path: "string",
+    extension: "string",
+  },
+  comics: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comic",
+    },
+  ],
+});
+
 module.exports = Character;
